ca8cdfca3149702f82bebf1df02dd58425bc3834
SEONG Park <b_ack2@naver.com>
2024-05-12 12:13:58 +0900

    play function add

--- result.js
+++ result.js
@@ -1,3 +1,4 @@
+//GET 키워드 받아오기
 const urlParams = new URLSearchParams(window.location.search);
 const keywords = [];
 for (let i = 1; i <= 3; i++) {
@@ -7,6 +8,7 @@ for (let i = 1; i <= 3; i++) {
     }
 }
 
+//일치하는 키워드만 필터링
 async function fetchSongs() {
     const response = await fetch('song.json');
     const data = await response.json();
@@ -23,6 +25,7 @@ async function filterSongsByAnyKeyword2Or3(keywords) {
     return songs.filter(song => keywords.some(keyword => song.keywords.includes(keyword)));
 }
 
+//키워드 출력
 async function displayFilteredSongs() {
     const songDetail = document.getElementById("songDetail");
     const similarDetail = document.getElementById("similarDetail");
@@ -30,6 +33,7 @@ async function displayFilteredSongs() {
     
     songDetailAlert.style.display = "none";
 
+    //완전 일치하는 노래
     const filteredSongs = await filterSongsByAllKeywords(keywords);
     const songListContainer = document.getElementById('songList');
     songListContainer.innerHTML = ""; 
@@ -40,14 +44,18 @@ async function displayFilteredSongs() {
             const songDiv = document.createElement('div');
             songDiv.classList.add('song-list', 'mb-3');
             songDiv.innerHTML = `
+            <div style="display: flex; align-items: center;">
                 <img src="${song.cover}" alt="${song.title} 앨범 커버" srcset="" class="album" />
-                &nbsp; &nbsp;
-                <span>
-                    <span style="font-size: 18px" class="bold">${song.title}</span>
+                <div style="margin-left: 10px;">
+                    <span style="font-size: 18px;" class="bold">${song.title}</span>
                     <br />
-                    <span style="font-size: 13px">${song.album} · ${song.year}</span>
-                </span>
-            `;
+                    <span style="font-size: 13px;">${song.album} · ${song.year}</span>
+                </div>
+            </div>
+            <div style="margin-left: auto;">
+                <button onclick="playVideoWithId('${song.id}', this)" class="play"><i class="fa-solid fa-play"></i></button>
+            </div>
+        `;        
             songListContainer.appendChild(songDiv);
         });
     } else {
@@ -55,6 +63,7 @@ async function displayFilteredSongs() {
         songDetailAlert.style.display = "block";
     }
 
+    //비슷한 노래
     const filteredSongsByAnyKeyword2Or3 = await filterSongsByAnyKeyword2Or3(keywords.slice(1));
     const similarSongContainer = document.getElementById('similarSong');
     similarSongContainer.innerHTML = ""; 
@@ -66,26 +75,32 @@ async function displayFilteredSongs() {
             const songDiv = document.createElement('div');
             songDiv.classList.add('song-list', 'mb-3');
             songDiv.innerHTML = `
+            <div style="display: flex; align-items: center;">
                 <img src="${song.cover}" alt="${song.title} 앨범 커버" srcset="" class="album" />
-                &nbsp; &nbsp;
-                <span>
-                    <span style="font-size: 18px" class="bold">${song.title}</span>
+                <div style="margin-left: 10px;">
+                    <span style="font-size: 18px;" class="bold">${song.title}</span>
                     <br />
-                    <span style="font-size: 13px">${song.album} · ${song.year}</span>
-                </span>
-            `;
+                    <span style="font-size: 13px;">${song.album} · ${song.year}</span>
+                </div>
+            </div>
+            <div style="margin-left: auto;">
+                <button onclick="playVideoWithId('${song.id}', this)" class="play"><i class="fa-solid fa-play"></i></button>
+            </div>
+        `;        
             similarSongContainer.appendChild(songDiv);
         });
     } else {
         similarDetail.style.display = "none";
     }
     
+    //갯수/시간 표시하는 부분
     const countSongs = document.getElementById("count");
     const countTime = document.getElementById("time");
     const value =  filteredSongsByAnyKeyword2Or3.length + filteredSongs.length;
 
     countSongs.innerHTML = value;
 
+    //시간 계산
     const time = value * 3;
     if(time > 60) {
         const displayTime = Math.round(time / 60);
@@ -95,4 +110,61 @@ async function displayFilteredSongs() {
     }
 }
 
-displayFilteredSongs();
+displayFilteredSongs(); 
+
+//유튜브 영상 출력
+let player;
+let currentPlayerId; 
+
+function refreshPage() {
+    location.reload();
+}
+
+function playVideoWithId(videoId, button) {
+    let isPlaying = false; 
+        var tag = document.createElement("script");
+        tag.src = "https://www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName("script")[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+        function onPlayerReady(event) {
+            event.target.setPlaybackQuality("small");
+            player = event.target;
+            button.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+            currentPlayerId = button.id;
+            isPlaying = true;
+        }
+
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING) {
+                isPlaying = true;
+            } else {
+                isPlaying = false;
+            }
+        }
+
+        window.onYouTubeIframeAPIReady = function () {
+            player = new YT.Player("player", {
+                height: "0",
+                width: "0",
+                videoId: videoId,
+                playerVars: {
+                    autoplay: 1,
+                    controls: 0,
+                    showinfo: 0,
+                    modestbranding: 1,
+                    loop: 1,
+                    fs: 0,
+                    cc_load_policy: 0,
+                    iv_load_policy: 3,
+                    autohide: 0,
+                },
+                events: {
+                    onReady: onPlayerReady,
+                    onStateChange: onPlayerStateChange,
+                },
+            });
+        };
+
+    button.onclick = refreshPage;
+}
